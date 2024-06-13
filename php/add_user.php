41df750d833efc72ce4617b40779eb766ce72db3
Olia <oliaopanasiukk@gmail.com>
2024-06-13 17:26:22 +0300

    added logic for adding new users to the database

---
+++ php/add_user.php
@@ -0,0 +1,25 @@
+<?php
+$bd_musicApp = mysqli_connect('localhost', 'root', '', 'music_application');
+
+if (!$bd_musicApp) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$fullName = $_POST['fullName'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$password = $_POST['password'];
+ 
+$sql = "INSERT INTO users (FullName, Username, Email, Phone, Password)
+ VALUES ('$fullName', '$username', '$email', '$phone', '$password')";
+
+if (mysqli_query($bd_musicApp, $sql)) {
+    echo "New user added successfully";
+    header("Location: ../pages/index.html");
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($bd_musicApp);
+}
+
+mysqli_close($bd_musicApp);
+?>  
